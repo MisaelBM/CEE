e2301d1a216480be6230d7160c14cc8d61b9af5e
Misael Morgado <112130147+MisaelBM@users.noreply.github.com>
2024-08-07 12:47:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ document.getElementById("menuButton").addEventListener('click', () => {
     for (let i = 1; i <= 3; i++) {
         clickMenu ? document.querySelector(`.menu-bar-${i}`).classList.remove(`menu-bar-anim-${i}`) : document.querySelector(`.menu-bar-${i}`).classList.add(`menu-bar-anim-${i}`);
     };
+    clickMenu ? document.querySelector(".menu-display-content").classList.remove("menu-display-anim") : document.querySelector(".menu-display-content").classList.add("menu-display-anim"); 
     clickMenu = clickMenu ? false : true;
 });
+setTimeout (
+    function () {
+        document.getElementById("mainHome").style.display = "flex";
+        document.getElementById("screenStart").style.display = "none";
+    }
+, 1000);
